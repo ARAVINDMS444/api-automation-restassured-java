64435a3e43bab2ddf0327099c25e5a6e40eb2de4
Aravind MS <aravind.ms4444@gmail.com>
2025-08-08 18:19:24 +0530

    Commit 3

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -1,81 +1,79 @@
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.*;
-import io.restassured.response.Response;
 import static io.restassured.RestAssured.*;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ApiTests {
 
-    @Test
-    @Order(1)
-    public void getRequestTest(){
-        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
-       Response response = given().when().get("/posts/1");
+  @Test
+  @Order(1)
+  public void getRequestTest() {
+    RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    Response response = given().when().get("/posts/1");
 
-       int statusCode = response.getStatusCode();
-       String title = response.jsonPath().getString("title");
+    int statusCode = response.getStatusCode();
+    String title = response.jsonPath().getString("title");
 
-       Assertions.assertEquals(200, statusCode);
-       Assertions.assertEquals("sunt aut facere repellat provident occaecati excepturi optio reprehenderit", title);
-       System.out.println("GET REQUEST PASSED");
-    }
+    Assertions.assertEquals(200, statusCode);
+    Assertions.assertEquals(
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit", title);
+    System.out.println("GET REQUEST PASSED");
+  }
 
-    @Test
-    @Order(2)
-    public void postRequestTest() {
-        RestAssured.baseURI = "https://reqres.in/";
+  @Test
+  @Order(2)
+  public void postRequestTest() {
+    RestAssured.baseURI = "https://reqres.in/";
 
-        Response response = given()
-                .header("Content-Type", "application/json")
-                .header("x-api-key", "reqres-free-v1")
-                .body("{\n" +
-                        "    \"name\": \"morpheus\",\n" +
-                        "    \"job\": \"leader\"\n" +
-                        "}")
-                .when()
-                .post("/api/users");
+    Response response =
+        given()
+            .header("Content-Type", "application/json")
+            .header("x-api-key", "reqres-free-v1")
+            .body("{\n" + "    \"name\": \"morpheus\",\n" + "    \"job\": \"leader\"\n" + "}")
+            .when()
+            .post("/api/users");
 
-        int statusCode = response.getStatusCode();
-        String name = response.jsonPath().getString("name");
+    int statusCode = response.getStatusCode();
+    String name = response.jsonPath().getString("name");
 
-        Assertions.assertEquals(201, statusCode);
-        Assertions.assertEquals("morpheus", name);
-        System.out.println("POST REQUEST PASSED");
-    }
+    Assertions.assertEquals(201, statusCode);
+    Assertions.assertEquals("morpheus", name);
+    System.out.println("POST REQUEST PASSED");
+  }
 
-    @Test
-    @Order(3)
-    public void putRequestTest() {
-        RestAssured.baseURI = "https://reqres.in/";
+  @Test
+  @Order(3)
+  public void putRequestTest() {
+    RestAssured.baseURI = "https://reqres.in/";
 
-        Response response = given()
-                .header("Content-Type", "application/json")
-                .header("x-api-key", "reqres-free-v1")
-                .body("{\n" +
-                        "    \"name\": \"morpheus\",\n" +
-                        "    \"job\": \"zion resident\"\n" +
-                        "}")
-                .when()
-                .put("/api/users/2");
+    Response response =
+        given()
+            .header("Content-Type", "application/json")
+            .header("x-api-key", "reqres-free-v1")
+            .body(
+                "{\n" + "    \"name\": \"morpheus\",\n" + "    \"job\": \"zion resident\"\n" + "}")
+            .when()
+            .put("/api/users/2");
 
-        int statusCode = response.getStatusCode();
-        String job = response.jsonPath().getString("job");
+    int statusCode = response.getStatusCode();
+    String job = response.jsonPath().getString("job");
 
-        Assertions.assertEquals(200, statusCode);
-        Assertions.assertEquals("zion resident", job);
-        System.out.println("PUT REQUEST PASSED");
-    }
+    Assertions.assertEquals(200, statusCode);
+    Assertions.assertEquals("zion resident", job);
+    System.out.println("PUT REQUEST PASSED");
+  }
 
-    @Test
-    @Order(4)
-    public void deleteRequestTest(){
-        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
-        Response response = when().delete("/posts/1");
+  @Test
+  @Order(4)
+  public void deleteRequestTest() {
+    RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    Response response = when().delete("/posts/1");
 
-        int statusCode = response.getStatusCode();
+    int statusCode = response.getStatusCode();
 
-        Assertions.assertEquals(200, statusCode);
-        System.out.println("DELETE REQUEST PASSED");
-    }
+    Assertions.assertEquals(200, statusCode);
+    System.out.println("DELETE REQUEST PASSED");
+  }
 }
